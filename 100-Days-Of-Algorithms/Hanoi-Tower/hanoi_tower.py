4e4341162520f072b90d03760620e59c551b4cb4
xuhang57 <xuhang57@users.noreply.github.com>
2019-03-28 22:31:15 -0400

    edit the code

--- 100-Days-Of-Algorithms/Hanoi-Tower/hanoi_tower.py
+++ 100-Days-Of-Algorithms/Hanoi-Tower/hanoi_tower.py
@@ -2,10 +2,11 @@ def HanoiTower(height, source, dest, aux):
 	if height == 1:
 	    print ("Move disk 1 from source", source, "to", dest)
 	    return
-	hanoi_tower(height-1, source, aux, dest)
+	HanoiTower(height-1, source, aux, dest)
 	print ("Move disk", height, "from source", source, "to", dest)
-	hanoi_tower(height-1, aux, dest, source)
+	HanoiTower(height-1, aux, dest, source)
 
 height = 3
 HanoiTower(height, "A", "B", "C")
-
+height = 4
+HanoiTower(height, "1", "2", "3")
